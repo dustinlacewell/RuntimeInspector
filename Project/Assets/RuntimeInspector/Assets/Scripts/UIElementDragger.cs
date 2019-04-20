c18c087d032f8c80a10256b56ffa48992c766b25
Dustin Lacewell <dlacewell@gmail.com>
2019-04-20 14:39:50 -0500

    Fixed dragging offset

--- Project/Assets/RuntimeInspector/Assets/Scripts/UIElementDragger.cs
+++ Project/Assets/RuntimeInspector/Assets/Scripts/UIElementDragger.cs
@@ -6,16 +6,19 @@ public class UIElementDragger : EventTrigger
 
     private bool dragging;
 
+    private Vector2 offset;
+
     public void Update()
     {
         if (dragging)
         {
-            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y) + offset;
         }
     }
 
     public override void OnPointerDown(PointerEventData eventData)
     {
+        offset = transform.position - new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
         dragging = true;
     }
 
